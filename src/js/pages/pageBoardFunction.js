5e1d234bc31e2fbb6c6130f29a06da13488d44e9
santi <beishuitang@163.com>
2020-07-02 02:36:36 +0800

    修复board页未登录导致的找不到元素错误提示

--- src/js/pages/pageBoardFunction.js
+++ src/js/pages/pageBoardFunction.js
@@ -4,7 +4,7 @@ export default function () {
     let bodyEl = document.getElementById('body');
     let tableEl = bodyEl.querySelector('table');
     let userEls = document.querySelectorAll('#body table .title_12');
-    if (config.onMobile) {
+    if (config.onMobile && tableEl != null) {
         splitName(userEls);
         relayTableAll(tableEl, [[3, 2]], [0, 2, 4, 5, 6, 7, 8]);
         // splitTableAll(tableEl, [[1, 8]], [0, 4, 5, 6, 8]);
