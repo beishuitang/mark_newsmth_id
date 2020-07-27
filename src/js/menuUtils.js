a724049ae27eda3d26e8947aea435a08d6af2e59
santi <beishuitang@163.com>
2020-07-28 00:46:53 +0800

    记录密码

--- src/js/menuUtils.js
+++ src/js/menuUtils.js
@@ -21,18 +21,24 @@ export default {
         }
         menu.remove();
         this.onMenuMut();
-        mut(menu, { attributes: false, childList: true, subtree: false }, this.onMenuMut);
+        mut(newMenu, { attributes: true, childList: false, subtree: false }, () => {
+            this.onMenuMut();
+        });
     },
     onMut: function () {
         config.menuConfig.showMenu = false;
     },
     onMenuMut: function () {
-        // this.savePassword();
+        this.savePassword();
         this.addSettingMenu();
     },
     addSettingMenu: function () {
+        if (document.querySelector('#setting') != null) {
+            return;
+        }
         let ul = document.querySelector('#menu #xlist>ul');
         let li = ul.querySelector('li').cloneNode(true);
+        li.setAttribute('id', 'setting');
         li.querySelector('ul').remove();
         li.querySelector('.toggler').style = 'background-position:-255px -20px';
         li.querySelector('a').innerText = '设置选项';
@@ -42,21 +48,25 @@ export default {
         }
         ul.appendChild(li);
     },
-    savepassword: function () {
+    savePassword: function () {
         let ifAutoFillPassword = config.menuConfig.autoFillPassword;
         let u_id = document.querySelector('#u_login_id');
         let u_passwd = document.querySelector('#u_login_passwd');
-        if (ifAutoFillPassword && u_id != null && u_passwd != null) {
-            let storageIdKey = config.storageKeys.STORAGE_ID;
-            let storagePasswdKey = config.storageKeys.STORAGE_PASSWORD;
+        let u_login_submit = document.querySelector('#u_login_submit');
+        let storageIdKey = config.storageKeys.STORAGE_ID;
+        let storagePasswdKey = config.storageKeys.STORAGE_PASSWORD;
+        if (!ifAutoFillPassword) {
+            localStorage.setItem(storagePasswdKey, '');
+            localStorage.setItem(storageIdKey, '');
+        }
+        if (ifAutoFillPassword && u_id != null && u_passwd != null && !u_id.getAttribute('watched')) {
+            u_id.setAttribute('watched', true);
             u_id.value = localStorage.getItem(storageIdKey);
             u_passwd.value = localStorage.getItem(storagePasswdKey);
-            u_id.onchange = function () {
-                localStorage.setItem(storageIdKey, u_id.value);
-            }
-            u_passwd.onchange = function () {
+            u_login_submit.addEventListener('click', function () {
                 localStorage.setItem(storagePasswdKey, u_passwd.value);
-            }
+                localStorage.setItem(storageIdKey, u_id.value);
+            })
         }
     },
 }
