8ed2dcec328a8547818f42f685f1e3f502546e97
santi <beishuitang@163.com>
2020-08-03 00:10:17 +0800

    增加panel搜索功能

--- src/components/tags/Panel.vue
+++ src/components/tags/Panel.vue
@@ -1,15 +1,26 @@
 <template>
   <transition name="slide-right">
-    <div class="smth_mark_id_panel" v-show="panelConfig.showPanel">
-      <div class="boder" v-for="(userData,id) in usersData" :key="id">
-        <h3>{{id}}</h3>
-        <UserTags :tags="userData.tags"></UserTags>
+    <div
+      style="height:100%;min-width:13rem"
+      class="smth_mark_id_panel"
+      v-show="panelConfig.showPanel"
+    >
+      <div>
+        <div class="boder" v-for="(userData,id) in filtedUsersData" :key="id">
+          <h3>{{id}}</h3>
+          <UserTags :tags="userData.tags"></UserTags>
+        </div>
+        <br />
+      </div>
+      <div class="search">
+        <input type="text" placeholder="搜索" v-model="searchText" />
       </div>
     </div>
   </transition>
 </template>
 
 <script>
+// TODO 搜索功能
 import UserTags from "./UserTags";
 export default {
   name: "Panel",
@@ -18,6 +29,30 @@ export default {
     msg: String,
     usersData: Object,
     panelConfig: Object,
+  },
+  data: function () {
+    return {
+      searchText: "",
+    };
+  },
+  computed: {
+    filtedUsersData: function () {
+      let result = {};
+      Object.keys(this.usersData).forEach((key) => {
+        let userData = this.usersData[key];
+        if (key.match(this.searchText)) {
+          result[key] = userData;
+        } else {
+          let tags = userData.tags;
+          Object.keys(tags).forEach((tagName) => {
+            if (tagName.match(this.searchText)) {
+              result[key] = userData;
+            }
+          });
+        }
+      });
+      return result;
+    },
   },
 };
 </script>
@@ -28,16 +63,20 @@ export default {
   bottom: 0;
   background-color: bisque;
   right: 0;
-  max-height: 100%;
   overflow-y: scroll;
   z-index: 1;
 }
 
 .boder {
   border: double;
+  padding: 0.5rem 1rem 0.5rem 1rem;
+}
+.search {
+  position: fixed;
+  bottom: 0;
 }
 .slide-right-enter-active {
-  right: -10rem;
+  right: -100%;
   transition: all 0.5s;
 }
 .slide-right-leave-active {
@@ -51,6 +90,6 @@ export default {
 }
 .slide-right-enter,
 .slide-right-leave-to {
-  right: -10rem;
+  right: -100%;
 }
 </style>
