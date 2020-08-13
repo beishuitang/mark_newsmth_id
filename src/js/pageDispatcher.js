e1aaaf449c8848d6bb3ee15eecaecf34f9044b93
santi <beishuitang@163.com>
2020-08-13 21:58:43 +0800

    调整搜索文章页面显示布局

--- src/js/pageDispatcher.js
+++ src/js/pageDispatcher.js
@@ -2,13 +2,16 @@ import mainData from './mainData'
 import pageArticleFunction from './pages/pageArticleFunction'
 import pageBoardFunction from './pages/pageBoardFunction'
 import pageMainpageFunction from './pages/pageMainpageFunction'
+import pageSearchFunction from './pages/pageSearchFunction'
 export default {
     dispatch: function () {
         switch (mainData.mainHash) {
             case 'article':
                 pageArticleFunction();
                 break;
-
+            case 's':
+                pageSearchFunction();
+                break;
             case 'board':
                 pageBoardFunction();
                 break;
---
+++ src/js/pages/pageSearchFunction.js
@@ -0,0 +1,27 @@
+import config from '../../config/config'
+import mainData from '../mainData'
+import { splitName, relayTableAll } from '../commonUtils'
+import browserUtil from '../browseUtil'
+export default function () {
+    let sReg = /nForum\/#!s\/(\w+)\?/
+    let m = mainData.currentPageHref.match(sReg);
+    if (m[1] == 'article') {
+        let bodyEl = document.getElementById('body');
+        let tableEl = bodyEl.querySelector('table');
+        let userEls = document.querySelectorAll('#body table .title_12');
+        if (config.onMobile && tableEl != null) {
+            splitName(userEls);
+            relayTableAll(tableEl, [[4, 3]], [0, 1, 3, 5, 6, 7]);
+        }
+        let topicTrs = document.querySelectorAll('.board-list>tbody>tr');
+        mainData.topicLinks.splice(0, mainData.topicLinks.length);
+        for (let index = 0; index < topicTrs.length; index++) {
+            let topic_el = topicTrs[index].querySelector('.title_9>a');
+            let pos_el = topicTrs[index].querySelectorAll('.middle')[0];
+            let pos = parseInt(pos_el.innerHTML);
+            console.log(topic_el)
+            console.log(pos)
+            browserUtil.addVisitedLinkStyle(topic_el, pos)
+        }
+    }
+}
