9083ceb46f8f81ee939418375c2bbf3ecd894fc2
santi <beishuitang@163.com>
2020-08-14 22:47:12 +0800

    列表出错时自动刷新

--- src/js/pages/pageBoardFunction.js
+++ src/js/pages/pageBoardFunction.js
@@ -6,23 +6,31 @@ export default function () {
     let bodyEl = document.getElementById('body');
     let tableEl = bodyEl.querySelector('table');
     let userEls = document.querySelectorAll('#body table .title_12');
-    if (config.onMobile && tableEl != null) {
-        splitName(userEls);
-        relayTableAll(tableEl, [[3, 2]], [0, 2, 4, 5, 6, 7, 8]);
-        // splitTableAll(tableEl, [[1, 8]], [0, 4, 5, 6, 8]);
-        // userEls.forEach((el) => {
-        // el.style.width = '8rem';
-        // el.style['text-align'] = 'center';
-        // })
-    }
     let topicTrs = document.querySelectorAll('.board-list>tbody>tr');
-    // let topicTrs = document.querySelectorAll('.board-list .title_9>a');
-    mainData.topicLinks.splice(0, mainData.topicLinks.length);
-    for (let index = 0; index < topicTrs.length; index++) {
-        let topic_el = topicTrs[index].querySelector('.title_9>a');
-        // let a_href = topic_el.href;
-        let pos_el = topicTrs[index].querySelectorAll('.middle')[2];
-        let pos = parseInt(pos_el.innerHTML);
-        browserUtil.addVisitedLinkStyle(topic_el, pos)
+    if (topicTrs.length > 1) {
+        sessionStorage.setItem('iReload', 0);
+        if (config.onMobile && tableEl != null) {
+            splitName(userEls);
+            relayTableAll(tableEl, [[3, 2]], [0, 2, 4, 5, 6, 7, 8]);
+        }
+        mainData.topicLinks.splice(0, mainData.topicLinks.length);
+        for (let index = 0; index < topicTrs.length; index++) {
+            let topic_el = topicTrs[index].querySelector('.title_9>a');
+            // let a_href = topic_el.href;
+            let pos_el = topicTrs[index].querySelectorAll('.middle')[2];
+            let pos = parseInt(pos_el.innerHTML);
+            browserUtil.addVisitedLinkStyle(topic_el, pos)
+        }
+    } else {
+        let i = sessionStorage.getItem('iReload');
+        i = i ? parseInt(i) : 0;
+        if (i < 5) {
+            sessionStorage.setItem('iReload', i + 1);
+            setTimeout(() => {
+                window.location.reload(true);
+            }, 500);
+        }
     }
+
+
 }
